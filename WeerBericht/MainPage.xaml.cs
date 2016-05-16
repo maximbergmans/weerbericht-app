5c47ab449119099cd7c76dfe201fa8eeef46e265
Maxim Bergmans <bergmansmaxim@hotmail.com>
2016-05-16 21:00:25 +0200

    versie 1.3

---
+++ WeerBericht/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace WeerBericht
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var locator = new Geolocator();
+            locator.DesiredAccuracyInMeters = 50;
+
+            var position = await locator.GetGeopositionAsync();
+
+            await MyMap.TrySetViewAsync(position.Coordinate.Point, 18D);           
+
+        }       
+        private void getPositionButton_Click(object sender, RoutedEventArgs e)
+        {
+            /*string webresponse;
+            HttpClient http = new System.Net.Http.HttpClient();
+            HttpResponseMessage response = await http.GetAsync("http://api.openweathermap.org/data/2.5/weather?lat=50.907799&lon=5.4221&APPID=45910e37f9b3c1547078f7a23e0fad4c");
+            webresponse = await response.Content.ReadAsStringAsync();*/
+
+            positionTextBlock.Text = String.Format("{0}, {1}",
+                MyMap.Center.Position.Latitude,
+                MyMap.Center.Position.Longitude);
+
+        }
+        private async void setPositionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var myPosition = new Windows.Devices.Geolocation.BasicGeoposition();
+            myPosition.Latitude = 41.7446;
+            myPosition.Longitude = -087.7915;
+
+            var myPoint = new Windows.Devices.Geolocation.Geopoint(myPosition);
+            if (await MyMap.TrySetViewAsync(myPoint, 10D))
+            {
+
+            }
+
+        }
+
+        private void mySlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+        }
+    }
+}
