44943fb8123e350f8d072f7d73400ed04eb7ae29
Maxim Bergmans <bergmansmaxim@hotmail.com>
2016-05-17 16:10:04 +0200

    geoposition+pushpin op huidige locatie+api+parsing van de api informatie

--- WeerBericht/MainPage.xaml.cs
+++ WeerBericht/MainPage.xaml.cs
@@ -17,6 +17,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Globalization;
+using Windows.UI.Xaml.Shapes;
+using Windows.UI;
+using Windows.UI.Xaml.Controls.Maps;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -27,14 +31,19 @@ namespace WeerBericht
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        
+        NumberFormatInfo nfi = new NumberFormatInfo();
 
         public MainPage()
         {
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            nfi.NumberDecimalSeparator = ".";
         }
+
+        double latitude;
+        double longitude;
 
         /// <summary>
         /// Invoked when this page is about to be displayed in a Frame.
@@ -46,12 +55,15 @@ namespace WeerBericht
             var locator = new Geolocator();
             locator.DesiredAccuracyInMeters = 50;
 
-            var position = await locator.GetGeopositionAsync();
+            Geoposition position = await locator.GetGeopositionAsync();
+            latitude = position.Coordinate.Point.Position.Latitude;
+            longitude = position.Coordinate.Point.Position.Longitude;
 
             await MyMap.TrySetViewAsync(position.Coordinate.Point, 18D);
 
             mySlider.Value = MyMap.ZoomLevel;
 
+            AddMapIcon();
         }       
         private void getPositionButton_Click(object sender, RoutedEventArgs e)
         {
@@ -60,18 +72,7 @@ namespace WeerBericht
                 MyMap.Center.Position.Longitude);
 
         }
-        private async void setPositionButton_Click(object sender, RoutedEventArgs e)
-        {
-            var myPosition = new Windows.Devices.Geolocation.BasicGeoposition();
-            myPosition.Latitude = 41.7446;
-            myPosition.Longitude = -087.7915;
-
-            var myPoint = new Windows.Devices.Geolocation.Geopoint(myPosition);
-            if (await MyMap.TrySetViewAsync(myPoint, 10D))
-            {
-
-            }
-        }
+        
 
         private void mySlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
@@ -82,70 +83,23 @@ namespace WeerBericht
         }
 
         private async void Resp_btn_Click(object sender, RoutedEventArgs e)
-        {         
+        {
+                     
             HttpClient client = new HttpClient();
-            var data = await client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?lat=50.907799&lon=5.4221&APPID=45910e37f9b3c1547078f7a23e0fad4c");           
+            //var data = await client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?lat=50.907799&lon=5.4221&APPID=45910e37f9b3c1547078f7a23e0fad4c");
+            var data = await client.GetStringAsync("http://api.openweathermap.org/data/2.5/weather?lat="+Convert.ToString(latitude,nfi)+"&lon="+ Convert.ToString(longitude, nfi) +"&APPID=45910e37f9b3c1547078f7a23e0fad4c");
             var weatherList = JsonConvert.DeserializeObject<weather>(data);
             positionTextBlock.Text = "Temperature: " + Convert.ToString(weatherList.main.temp-272.15)+ " ° Celsius";
 
         }
-        public class Coord
-        {
-            public double lon { get; set; }
-            public double lat { get; set; }
-        }
 
-        public class Weather
+        private void AddMapIcon()
         {
-            public int id { get; set; }
-            public string main { get; set; }
-            public string description { get; set; }
-            public string icon { get; set; }
-        }
-
-        public class Main
-        {
-            public double temp { get; set; }
-            public double pressure { get; set; }
-            public int humidity { get; set; }
-            public double temp_min { get; set; }
-            public double temp_max { get; set; }
-            public double sea_level { get; set; }
-            public double grnd_level { get; set; }
-        }
-
-        public class Wind
-        {
-            public double speed { get; set; }
-            public double deg { get; set; }
-        }
-
-        public class Clouds
-        {
-            public int all { get; set; }
-        }
-
-        public class Sys
-        {
-            public double message { get; set; }
-            public string country { get; set; }
-            public int sunrise { get; set; }
-            public int sunset { get; set; }
-        }
-
-        public class weather
-        {
-            public Coord coord { get; set; }
-            public List<Weather> weatherapp { get; set; }
-            public string @base { get; set; }
-            public Main main { get; set; }
-            public Wind wind { get; set; }
-            public Clouds clouds { get; set; }
-            public int dt { get; set; }
-            public Sys sys { get; set; }
-            public int id { get; set; }
-            public string name { get; set; }
-            public int cod { get; set; }
+            MapIcon mapicon1 = new MapIcon();
+            mapicon1.Location = new Geopoint(new BasicGeoposition() { Latitude = Convert.ToDouble(latitude, nfi) , Longitude = Convert.ToDouble(longitude) });
+            mapicon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+            mapicon1.Title = "your position";
+            MyMap.MapElements.Add(mapicon1);
         }
     }
 }
